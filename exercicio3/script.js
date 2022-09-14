5c0dc854debe9fbf15720c889c7ddc95bbf6e340
VeltoPD <veltopd@gmail.com>
2022-09-14 16:27:49 -0300

    Entrega dos exercícios a017

---
+++ exercicio3/script.js
@@ -0,0 +1,31 @@
+function soma() {
+    let v1 = +prompt("valor 1")
+    let v2 = +prompt("valor 2")
+    let sum = (v1 + v2)
+    return sum
+}
+console.log(soma())
+
+function subtração() {
+    let v1 = +prompt("valor 1")
+    let v2 = +prompt("valor 2")
+    let subtraction = (v1 - v2)
+    return subtraction
+}
+console.log(subtração())
+
+function multiplicação() {
+    let v1 = +prompt("valor 1")
+    let v2 = +prompt("valor 2")
+    let multiplication = (v1 * v2)
+    return multiplication
+}
+console.log(multiplicação())
+
+function divisão() {
+    let v1 = +prompt("valor 1")
+    let v2 = +prompt("valor 2")
+    let division = (v1 / v2)
+    return division
+}
+console.log(divisão())
